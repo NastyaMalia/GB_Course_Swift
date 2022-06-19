e2bd6534147083c5124a8e8c1c035dc6a04bbda4
Nastya <Nastusya.kazakova@gmail.com>
2022-06-19 13:26:34 +0300

    Add VK project

---
+++ Vkontakte/Vkontakte/LogInConroller.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Vkontakte
+//
+//  Created by Nastya Kazakova on 15.06.22.
+//
+
+import UIKit
+
+class LogInConroller: UIViewController {
+
+    @IBOutlet weak var textLogIn: UITextField!
+    
+    @IBOutlet weak var textPasword: UITextField!
+    
+    @IBOutlet weak var imgLogo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    }
+
+    
+    @IBAction func logInButton(_ sender: Any) {
+    }
+    
+}
+
