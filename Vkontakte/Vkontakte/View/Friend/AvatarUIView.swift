d342482924453e7b156833f813427edd693d5139
Nastya <Nastusya.kazakova@gmail.com>
2022-06-30 15:34:45 +0300

    Create a shadow on avatar

---
+++ Vkontakte/Vkontakte/View/Friend/AvatarUIView.swift
@@ -0,0 +1,34 @@
+//
+//  AvatarUIView.swift
+//  Vkontakte
+//
+//  Created by Nastya Kazakova on 30.06.22.
+//
+
+import UIKit
+
+class AvatarUIView: UIView {
+
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var shadowView: UIView!
+
+    
+    var shadowColor = UIColor.black
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        shadowView.layer.shadowColor = shadowColor.cgColor
+        shadowView.layer.shadowOffset = .zero
+        shadowView.layer.shadowRadius = 7
+        shadowView.layer.shadowOpacity = 0.8
+       // shadowView.layer.backgroundColor =
+    }
+    
+    override func layoutSubviews() {
+        avatarImageView.layer.cornerRadius = bounds.height / 2
+        shadowView.layer.cornerRadius = bounds.height / 2
+        
+    }
+    
+}
